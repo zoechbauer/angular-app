f4aa45e4599058e4703e0464871123245bdd1772
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-04 17:42:20 +0100

    setup angular project with bootstrap 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+// tslint:disable-next-line: quotemark
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'my-first-app';
-}
+export class AppComponent {}
