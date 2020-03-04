e155409f214378f54f878e19c085af4096a06bd7
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-04 20:03:55 +0100

    Creating an inline template of the component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+    <p>
+      Inline templates should have only a few lines of code<br />
+      and the key for inline template is template and not templateUrl
+    </p>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
