f57ff369ae565fc45783b0fdd23cdb38ddbf8ee2
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-05 10:33:26 +0100

    attribute selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: '[app-servers]',
   template: `
     <app-server></app-server>
     <app-server></app-server>
-    <p>
-      Inline templates should have only a few lines of code<br />
-      and the key for inline template is template and not templateUrl
-    </p>
   `,
   styleUrls: ['./servers.component.css']
 })
